ec7aacbeb2659c77ba2a047faa753f6d96b4066b
aeronflorida <aeronflorida@users.noreply.github.com>
2016-07-17 23:00:59 +0800

    Add files via upload

---
+++ clock.cpp
@@ -0,0 +1,307 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+
+{
+    for(int a = 1; a <2 ; a = a + 1)
+   
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10      |       2 | |"<<endl;
+            cout<<"\t\t\t| |          |         | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+             Sleep(500);
+            
+            }
+            system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10         /    2 | |"<<endl;
+            cout<<"\t\t\t| |            /       | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |               /    | |"<<endl;
+            cout<<"\t\t\t| |  9       *  /    3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9       *------ 3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9       *  \\    3 | |"<<endl;
+            cout<<"\t\t\t| |               \\    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |            \\       | |"<<endl;
+            cout<<"\t\t\t| |  8          \\    4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |          |         | |"<<endl;
+            cout<<"\t\t\t| |  8       |       4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |       /            | |"<<endl;
+            cout<<"\t\t\t| |  8   /           4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+
+
+system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9    /  *       3 | |"<<endl;
+            cout<<"\t\t\t| |     /              | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  9 ------*       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+           system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10              2 | |"<<endl;
+            cout<<"\t\t\t| |     \\              | |"<<endl;
+            cout<<"\t\t\t| |  9    \\  *       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+           system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10   \\          2 | |"<<endl;
+            cout<<"\t\t\t| |        \\           | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+           system("cls");
+            
+    
+    {
+            cout<<"\n\n\n\t\t\t\tClock\n"<<endl;
+            cout<<"\t\t\t ________________________ "<<endl;
+            cout<<"\t\t\t(  ____________________  )"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |     11   12   1    | |"<<endl;
+            cout<<"\t\t\t| |  10      |       2 | |"<<endl;
+            cout<<"\t\t\t| |          |         | |"<<endl;
+            cout<<"\t\t\t| |  9       *       3 | |"<<endl;
+            cout<<"\t\t\t| |                    | |"<<endl;
+            cout<<"\t\t\t| |  8               4 | |"<<endl;
+            cout<<"\t\t\t| |     7    6   5     | |"<<endl;
+            cout<<"\t\t\t| |____________________| |"<<endl;
+            cout<<"\t\t\t(________________________)"<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+
+    
+    
+    system("pause");
+    return 0;
+}
