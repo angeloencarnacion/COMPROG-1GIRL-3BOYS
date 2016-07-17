73928d4b93a6dbb569e79d87f4bb0872c9b52ab3
lawrence1326 <lawrencepogi1326@gmail.com>
2016-07-16 21:43:19 -0700

    Add files via upload

---
+++ pyramid.cpp
@@ -0,0 +1,269 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+
+{
+    for(int a = 1; a <2 ; a = a + 1)
+   
+    {
+            cout<<"\t\t\t	      /\\  	               "<<endl;
+            cout<<"\t\t\t             /__\\                    "<<endl;
+            cout<<"\t\t\t            /\\  /\\                  "<<endl;
+            cout<<"\t\t\t           /__\\/__\\                  "<<endl;
+            cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+}
+            system("cls");
+            
+    
+    {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t            /\\  /\\                  "<<endl;
+            cout<<"\t\t\t           /__\\/__\\                  "<<endl;
+            cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\ /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+}
+            system("cls");
+            
+            
+
+
+
+
+            
+    
+    {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+}
+
+system("cls");
+            
+    
+    {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+}
+
+system("cls");
+            
+    
+    {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+            
+}
+system("cls");
+            
+    
+     {
+            
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+            
+            
+}
+system("cls");
+
+            
+    
+     {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+}
+
+system("cls");
+
+            
+    
+    {
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+            cout<<"\n"<<endl;
+			cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+}
+
+
+system("cls");
+            
+    
+   {
+            
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\n"<<endl;
+			cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+}
+            
+            system("cls");
+            
+    
+    {
+            
+			cout<<"\n\n\n"<<endl;
+			cout<<"\t\t\t            /\\  /\\                  "<<endl;
+            cout<<"\t\t\t           /__\\/__\\                  "<<endl;
+            cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+            
+}
+           system("cls");
+            
+    
+    {
+            cout<<"\t\t\t	      /\\  	               "<<endl;
+            cout<<"\t\t\t             /__\\                    "<<endl;
+            cout<<"\t\t\t            /\\  /\\                  "<<endl;
+            cout<<"\t\t\t           /__\\/__\\                  "<<endl;
+            cout<<"\t\t\t          /\\  /\\  /\\             "<<endl;
+            cout<<"\t\t\t         /__\\/__\\/__\\            "<<endl;
+            cout<<"\t\t\t        /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t       /__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t      /\\  /\\  /\\  /\\  /\\            "<<endl;
+            cout<<"\t\t\t     /__\\/__\\/__\\/__\\/__\\                 "<<endl;
+            cout<<"\t\t\t    /\\  /\\  /\\  /\\  /\\  /\\                "<<endl;
+            cout<<"\t\t\t   /__\\/__\\/__\\/__\\/__\\/__\\               "<<endl;
+            cout<<"\t\t\t            "<<endl;
+            
+               Sleep(500);
+            
+            
+            
+}
+    
+    
+    system("pause");
+    return 0;
+}
